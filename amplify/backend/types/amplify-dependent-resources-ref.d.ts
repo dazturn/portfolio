8c8041ae77e3cef9b8e30609b424d1827ac136c7
Daziah Turner <daziahturner1@gmail.com>
2023-05-16 13:52:00 -0700

    GraphQL API deployed

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "portfolio": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "portfolioa37ba096": {
       "AppClientID": "string",
